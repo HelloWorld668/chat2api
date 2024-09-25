96c51d3b98ca53e3f52a748623244d41fcb5e780
123 <111@aas>
2024-09-25 17:49:19 +0800

    1

--- 1.sh
+++ 1.sh
@@ -1,4 +1,4 @@
-kill -9 $(lsof -t -i:8763)
+kill -9 $(lsof -t -i:8763) || true
 source venv/bin/activate
 export HISTORY_DISABLED=False
 export API_PREFIX=bNAKVo8HKje9U0djRVQD4GHBD8N42b65vRg0wEi6r1icZwsbL6
